64cf3127d774e7968b493d9c3d75c6e0dadfc036
edarraghford <33334026+edarraghford@users.noreply.github.com>
2018-09-06 04:25:42 +0200

    Add files via upload

--- spectrum.py
+++ spectrum.py
@@ -13,16 +13,23 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.patches as mpatches 
 import pynbody 
 
-ndata = 256 
-boxsize = 50.0 
-pixsize = boxsize/ndata  
+
+def getNyquistInteger(ndata): 
+    return int(ndata/2)  
+
+def generateLinearlySpacedFrequencies(nq): 
+    x = np.linspace(0,nq-1,nq)
+    return x 
+
+def generateLinearlySpacedGridFrequency(nq):  
+    x = generateLinearlySpacedFrequencies(nq)
+    return x, x, x 
 
 #Transforms 3D function back to frequency domain, averages spatially, 
 #and plots result spectrum against the input spectrum
-def plt_PSD_avg(fn,grid):
-    x = np.linspace(0,(ndata/2)-1,ndata/2)
-    y = np.linspace(0,(ndata/2)-1,ndata/2)
-    z = np.linspace(0,(ndata/2)-1,ndata/2)
+def plt_PSD_avg(fn,grid,ndata, pixsize):
+    nq = getNyquistInteger(ndata)
+    x, y, z = generateLinearlySpacedGridFrequency(nq)
 #    x = np.logspace(0,np.log10((ndata/2)),ndata/2)-1
 #    y = np.logspace(0,np.log10((ndata/2)),ndata/2)-1
 #    z = np.logspace(0,np.log10((ndata/2)),ndata/2)-1
@@ -34,17 +41,15 @@ def plt_PSD_avg(fn,grid):
 #compute power spectrum 
     pow = np.absolute(np.fft.fftn(fn,norm="ortho"))**2/ (2*np.pi)**3 /growth**2
     pow1 = np.absolute(np.fft.fftn(grid,norm="ortho"))**2/ (2*np.pi)**3 /growth**2
-    fx = rad.radial_data(pow1[:ndata/2,:ndata/2,:ndata/2], x=xx,y=yy,z=zz) 
-    fs = rad.radial_data(pow[:ndata/2,:ndata/2,:ndata/2], x=xx,y=yy,z=zz) #collapse to 1D spectrum  
-    fx.mean = fx.mean/np.average(fx.mean[1:ndata/2])
+    fx = rad.radial_data(pow1[:nq,:nq,:nq], x=xx,y=yy,z=zz) 
+    fs = rad.radial_data(pow[:nq,:nq,:nq], x=xx,y=yy,z=zz) #collapse to 1D spectrum  
+    fx.mean = fx.mean/np.average(fx.mean[1:nq])
     data = np.loadtxt("planck_2015_matterpower.dat") #load planck spectrum 
     fx.mean = fx.mean * 37 
 #plot both spectra 
     plt.xscale("log")
     plt.yscale("log") 
-    y1 = 2* np.pi / pixsize* np.fft.fftfreq(256,d=1) #frequency modes for plot 
-    print(y1[:ndata/2])
-    print("")
+    y1 = 2* np.pi / pixsize* np.fft.fftfreq(ndata,d=1) #frequency modes for plot 
 #    y1 = np.logspace(np.log10(y[0]),np.log10(y1[ndata/2]),ndata/2 )
 
     center = (y1[:-1] + y1[1:]) / 2
@@ -56,17 +61,16 @@ def plt_PSD_avg(fn,grid):
 #    mean, edges, number = stats.binned_statistic(y1[1:ndata/2], fs.mean[1:ndata/2], 'mean', bins = test)
 #    center = (edges[:-1] + edges[1:]) / 2 
 #    plt.plot(center, mean)    
-    plt.plot(center[1:ndata/2-1],fs.mean[1:ndata/2-1]) 
-    plt.plot(center[1:ndata/2],fx.mean[1:ndata/2])	    
-    plt.plot(data[:,0], data[:,1]) 
+    plt.plot(center[1:nq-1],fs.mean[1:nq-1], label='Simulated Power Spectrum') 
+    plt.plot(center[1:nq],fx.mean[1:nq], label= 'Simulated Power Spectrum (pre-gridded)')	    
+    plt.plot(data[:,0], data[:,1], label = 'Planck Power Spectrum') 
     plt.ylim([0.01,10000])
     plt.xlim([0.1,20])
     plt.xlabel("k(unit of k)") 
-    plt.title("Input Power Spectrum and Output Power Spectrum") 
-    blue_patch = mpatches.Patch(color='blue', label = 'Simulated Power Spectrum') 
-    orange_patch = mpatches.Patch(color='orange',label = 'Planck Power Spectrum') 
-    plt.legend(handles=[blue_patch,orange_patch]) 
-    plt.show()
+    plt.title("Input Power Spectrum and Output Power Spectrum")  
+    plt.legend()
+    plt.savefig('powerspectrum.png') 
+    plt.show() 
 
 #Divide simulated spectrum by physical spectrum, by first binning and averaging data
     bins = 60
@@ -77,18 +81,23 @@ def plt_PSD_avg(fn,grid):
     plt.title("simulated spectrum/actual spectrum") 
     plt.grid(color = 'b', linestyle = '--') 
     plt.scatter(x1,bin3) 
-    plt.show()
+    plt.savefig('residuals.png') 
+
 
 def average_spectrum(f):
     a =  np.sum(f)
     b =  a/(ndata)**3
     return (f/b)
 
-s = pynbody.load("snapshot_999.dms") #load simulation 
-f = pynbody.sph.to_3d_grid(s, 'rho', 256, 256, 256) 
+filename = sys.argv[1]  
+s = pynbody.load(filename) #load simulation 
+boxsize = s.properties['boxsize'].in_units("Mpc a h**-1")
+ndata = int(np.cbrt(len(s))) 
+pixsize = boxsize/ndata 
+f = pynbody.sph.to_3d_grid(s, 'rho', ndata, ndata, ndata) 
 h = f.in_units("Msol Mpc**-3 a**3 h**3") 
 h1 = average_spectrum(h) #divide by average density
 grid = np.load("grid-0.npy") 
 grid2 = average_spectrum(grid)
-p = plt_PSD_avg(h1,grid2)
+p = plt_PSD_avg(h1,grid2,ndata,pixsize)
 
